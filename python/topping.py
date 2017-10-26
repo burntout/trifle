07cbe86b388dce3195486508e54ac79ffa912124
Kate Dawson <dawsonka@chesterfield.ac.uk>
2017-10-26 14:50:25 +0100

    Project Euler question 26 - reptend primes

--- python/topping.py
+++ python/topping.py
@@ -21,4 +21,18 @@ For example, is Friday 13th more common than other days
 import datetime
 howmanyweekdays = lambda dayofmonth: [len(filter(lambda x: x==day, [datetime.date(year,month,dayofmonth).weekday() for year in range(2000,2400) for month in range(1,13)])) for day in range(0,7)]
 
+'''
+Calculate Reptend Primes for arbitrary base b
+reptend primes base b, or the primes p, for which b is a primitive root for the group Zmodp
 
+primes is a simple sieve of eratosthenes
+reptend_primes finds all primes for which b is not a primitive root n by calculating b**i or where b is a multiple of p
+and removes them from the list of primes.
+'''
+def primes(n):
+    def sieve(r):
+        return [] if r == [] else [r[0]] + sieve([e for e in r if e % r[0] != 0])
+    return sieve(range(2,n))
+
+def reptend_primes(n,b=10):
+    return set(primes(n)) - set([p for p in primes(n) if [j for j in range(1,p-1) if pow(b, j, p) == 1] or b % p == 0])
