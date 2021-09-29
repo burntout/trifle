6c7deba0d0eb1b4297a45661c10c6d17076cde69
Kate Dawson <kate@webarchitects.coop>
2021-09-29 01:29:45 +0100

    Generate sequences of relatively prime integers

--- haskell/RelPrime.hs
+++ haskell/RelPrime.hs
@@ -1,3 +1,7 @@
+-- If a and b are relatively prime
+-- then so is a*b + (b - a) (assuming b>a)
+-- example relPrimes 3 5 gives the Fermat primes
+
 relPrimes :: Integral a => a -> a -> [a]
 relPrimes a b = (a: iterate (\x -> x*(x-d) + d) b)
     where d = b - a
